b474cb5c553ff9374a301c428e423d0844c66276
1OAbabakir <shedo1222@gmail.com>
2024-05-09 17:10:02 +0200

    test client in 'run'

--- src/main/java/com/startApp.java
+++ src/main/java/com/startApp.java
@@ -1,10 +1,20 @@
 package com;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class startApp {
 
     public static void main(String[] args) {
         server mainServer = new server(994);
         System.out.println("End");
+
+        try (Socket first_client = new Socket("127.0.0.1", 994)) {
+        } catch (IOException e) {   
+            e.printStackTrace();
+        }
+
+
     }
 
 }
